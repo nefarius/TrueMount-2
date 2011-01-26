1657549b1935ed839c00900fc4a0f292e4272bd9
Benjamin Höglinger <nefarius@dhmx.at>
2011-01-26 20:38:03 +0000

    Latest release of all binaries with fixed updater UAC dialog

--- TrueMount/Properties/AssemblyInfo.cs
+++ TrueMount/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.19.88")]
-[assembly: AssemblyFileVersion("2.7.19.88")]
+[assembly: AssemblyVersion("2.7.19.92")]
+[assembly: AssemblyFileVersion("2.7.19.92")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
