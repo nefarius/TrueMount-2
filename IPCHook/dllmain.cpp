e302dd50327eb807a9a0427092dc058232e72dcf
Benjamin Höglinger <nefarius@dhmx.at>
2011-01-30 12:17:32 +0000

    Added more debug code Added error reporting via pipes

--- IPCHook/dllmain.cpp
+++ IPCHook/dllmain.cpp
@@ -16,6 +16,9 @@ volatile BOOL bHooked = FALSE;
 // The fake function
 int WINAPI MessageBoxHook(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType)
 {
+#ifdef _DEBUG
+	origMessageBoxW(NULL, L"Hooked", NULL, MB_ICONINFORMATION|MB_OK);
+#endif
 	wstring buffer(lpText);
 	HANDLE npipe;
 
@@ -44,7 +47,39 @@ BOOL APIENTRY DllMain(_In_ void * _HDllHandle, _In_ unsigned _Reason, _In_opt_ v
 	switch(_Reason)
 	{
 	case DLL_PROCESS_ATTACH:
+#ifdef _DEBUG
+		MessageBox(NULL, L"HOOKED", L"MAIN", MB_ICONINFORMATION|MB_OK);
+#endif
 		origMessageBoxW = nch.createHook(MessageBoxW, MessageBoxHook);
+
+		if(origMessageBoxW == NULL)
+		{
+#ifdef _DEBUG
+			MessageBox(NULL, L"Hook went wrong!", L"CRAP", MB_ICONINFORMATION|MB_OK);
+#endif
+			HANDLE npipe;
+			LPWSTR lpMsg = L"ERROR_HOOK";
+			// Send fail message back to parent process
+			if( WaitNamedPipe(L"\\\\.\\pipe\\TrueCryptMessage", 3000) )
+			{
+				npipe = CreateFile(L"\\\\.\\pipe\\TrueCryptMessage",
+					GENERIC_READ | GENERIC_WRITE,
+					0, NULL, OPEN_EXISTING, 0, NULL);
+				if( npipe != INVALID_HANDLE_VALUE )
+				{
+					DWORD dwRead;
+					WriteFile(npipe, (LPCVOID)lpMsg, sizeof(wchar_t) * wcslen(lpMsg), &dwRead, NULL);
+					CloseHandle(npipe);
+				}
+			}
+
+			// We can't do more, terminate
+			TerminateProcess(GetCurrentProcess(), ERROR_SUCCESS);
+		}
+
+#ifdef _DEBUG
+		origMessageBoxW(NULL, L"HOOKED2", L"RLY", MB_OK|MB_ICONINFORMATION);
+#endif
 		break;
 	case DLL_PROCESS_DETACH:
 		nch.removeHook(MessageBoxHook);
